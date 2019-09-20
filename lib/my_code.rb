c43e7f4dc39af92170acb74730bb9f3538be15e9
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-20 19:10:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
 # Your Code Here
+def map(array)
+  i=0
+  new_array = []
+  while i < array.length
+    new_array.push yield(array[i])
+    i+=1
+  end
+  new_array
+end
+
+def reduce(array, starting_point = 0)
+  i = 0
+  while i < array.length
+    yield(starting_point, array[i])
+    i+=1
+  end
+end
